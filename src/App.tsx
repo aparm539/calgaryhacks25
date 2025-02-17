c03cf839f13625fd30fce6eefaae4c46eeefdbfc
sunny <sunny.parmar@arcurve.com>
2025-02-17 16:31:26 -0700

    Update grid to fill page, resize columns based on window size and initialize with no placeholder data. Track keydown events and show in replay.

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,104 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 import VideoControl from './components/video-control'
 import Sheet from './components/spreadsheet' 
 import cursor from './assets/cursor.png'
+import { CellKeyDownEvent, RowNode } from 'ag-grid-community'
 
-interface MouseEvent{
+interface InterceptedEvent{ 
   id: string, 
   time: number
 }
 
-interface clickEvent extends MouseEvent{ 
-  element: EventTarget
+interface TargetedEvent extends InterceptedEvent{  
+  target: EventTarget
 }
 
-interface mouseMoveEvent extends MouseEvent{ 
+interface clickEvent extends TargetedEvent{ 
+}
+
+interface mouseMoveEvent extends InterceptedEvent{ 
   mouseX:number, 
   mouseY:number
 }
 
-const mouseEvents: MouseEvent[] = []
+interface keydownEvent extends TargetedEvent{ 
+  key: string, 
+  event: CellKeyDownEvent
+}
+
+const interceptedEvents: InterceptedEvent[] = []
 
 function App() {
-  let replayIndex: number = 0;
+  const gridRef = useRef();
   let startTime: number = 0;
+  let endTime: number = 0;
   let isRecording:boolean = false; 
   let isReplaying:boolean = false; 
   const [replayMouseX, setReplayMouseX]  = useState(0);
   const [replayMouseY, setReplayMouseY]  = useState(0);
   function handleClick(event: React.MouseEvent) { 
     if(!isRecording) return;
-    const newEvent:clickEvent = {id:crypto.randomUUID(), time: Date.now(), element:event.target}
-    mouseEvents.push(newEvent,)
+    const newEvent:clickEvent = {id:crypto.randomUUID(), time: Date.now(), target:event.target}
+    interceptedEvents.push(newEvent,)
   };
   function handleMouseMove(event:React.MouseEvent){ 
     if(!isRecording) return;
     const newMoveEvent:mouseMoveEvent ={id:crypto.randomUUID(), time: Date.now(), mouseX:event.clientX, mouseY:event.clientY} 
-    mouseEvents.push(newMoveEvent,)
-    console.log(mouseEvents.length)
+    interceptedEvents.push(newMoveEvent,)
   };
   function handleBtnClick(){ 
-    startTime = Date.now();
+    if (isRecording){ 
+      startTime= Date.now()
+    }
+    else{
+      endTime = Date.now(); 
+    }
     isRecording = !isRecording;
     console.log("Are we recording?: ", isRecording)
   }
   function replay_input(){
-    if (isReplaying || mouseEvents.length == 0) return;
+    if (isReplaying || interceptedEvents.length == 0) return;
     isReplaying = true;
     startTime = Date.now()
     replayNextInput(0);
 
   }
   function replayNextInput(index:number){ 
-    console.log(index)
-    if(index >=mouseEvents.length){ 
+    if(index >=interceptedEvents.length){ 
       isReplaying = false;
       return;
     }; 
 
-    if(index < mouseEvents.length-1){
-      let moveEvent = mouseEvents[index] as mouseMoveEvent
+    if(index < interceptedEvents.length-1){
+      const moveEvent = interceptedEvents[index] as mouseMoveEvent
+      const keypressEvent = interceptedEvents[index] as keydownEvent
       if (moveEvent.mouseX){ 
         setReplayMouseX(moveEvent.mouseX)
         setReplayMouseY(moveEvent.mouseY)
       }
-      let delay = mouseEvents[index +1 ].time - mouseEvents[index].time;
+      if(keypressEvent.key){ 
+        const dataField = keypressEvent.event.colDef.field
+        const oldValue:string = keypressEvent.event.data[dataField]
+        const newValue:string = oldValue.concat(keypressEvent.key)
+        keypressEvent.event.node.setDataValue(dataField,newValue)
+      }
+      const delay = interceptedEvents[index +1 ].time - interceptedEvents[index].time;
       setTimeout(() => replayNextInput(index+1), delay);
     }
   }
+  function onCellValueChanged(event: CellKeyDownEvent){
+      const keypress: keydownEvent = {id: crypto.randomUUID(), time: Date.now(), key: event.event.key, target: event.event.target, event: event} 
+      //console.log(event.node.setDataValue(event.colDef.field,"testing123"))
+      //console.log(gridRef.current.api.getRowNode(event.colDef.field))
+      interceptedEvents.push(keypress,)
+  }
   return (
-    < div onMouseMove={(e) => handleMouseMove(e)}>
+    < div onMouseMove={(e) => handleMouseMove(e)} style={{height: '100vh'}}>
       <img src={cursor} id="mouse" style={{left:replayMouseX, top:replayMouseY}}/> 
-      <textarea id="21"onClick={(e) => handleClick(e)}> </textarea>
       <button onClick={handleBtnClick}>Start/Stop</button>
       <button onClick={replay_input}>Start/Stop replay</button>
-      <Sheet/>
+      <Sheet onCellValueChangedCallback={onCellValueChanged} gridRef={gridRef}/>
       <VideoControl/> 
     </div>
 
--- src/components/spreadsheet.tsx
+++ src/components/spreadsheet.tsx
@@ -1,41 +1,168 @@
 import { AgGridReact } from 'ag-grid-react';
-import { AllCommunityModule, CellEditingStartedEvent, CellKeyDownEvent, GetRowIdParams, ModuleRegistry } from 'ag-grid-community'; 
+import { AllCommunityModule,GridSizeChangedEvent, ModuleRegistry, themeQuartz } from 'ag-grid-community'; 
 import { useCallback, useState } from "react";
 
 // Register all Community features
 ModuleRegistry.registerModules([AllCommunityModule]);
 
-
  
-function Sheet() {
+function Sheet({onCellValueChangedCallback, gridRef}) {
+        const theme = themeQuartz.withParams({
+            columnBorder:true
+        });
 
         // Row Data: The data to be displayed.
         const [rowData, setRowData] = useState([
-            { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-            { make: "Ford", model: "F-Series", price: 33850, electric: false },
-            { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''},
+            {id: '', value: ''}
         ]);
     
         // Column Definitions: Defines the columns to be displayed.
         const [colDefs, setColDefs] = useState([
-            { field: "make", editable: true },
-            { field: "model", editable: true },
-            { field: "price", editable: true },
-
+            { field: "A", editable: true,minWidth: 80 },
+            { field: "B", editable: true,minWidth: 80 },
+            { field: "C", editable: true,minWidth: 80 },
+            { field: "D", editable: true,minWidth: 80 },
+            { field: "E", editable: true,minWidth: 80 },
+            { field: "F", editable: true,minWidth: 80 },
+            { field: "G", editable: true,minWidth: 80 },
+            { field: "H", editable: true,minWidth: 80 },
+            { field: "I", editable: true,minWidth: 80 },
+            { field: "J", editable: true,minWidth: 80 },
+            { field: "K", editable: true,minWidth: 80 },
+            { field: "L", editable: true,minWidth: 80 },
+            { field: "M", editable: true,minWidth: 80 },
+            { field: "N", editable: true,minWidth: 80 },
+            { field: "O", editable: true,minWidth: 80 },
+            { field: "P", editable: true,minWidth: 80 },
         ]);
-        function onCellValueChanged(event: CellKeyDownEvent){
-             console.log(event)
 
-        }
+        const onGridSizeChanged = useCallback(
+            (params: GridSizeChangedEvent) => {
+              // get the current grids width
+              const gridWidth =
+                document.querySelector(".ag-body-viewport")!.clientWidth;
+              // keep track of which columns to hide/show
+              const columnsToShow = [];
+              const columnsToHide = [];
+              // iterate over all columns (visible or not) and work out
+              // now many columns can fit (based on their minWidth)
+              let totalColsWidth = 0;
+              const allColumns = params.api.getColumns();
+              if (allColumns && allColumns.length > 0) {
+                for (let i = 0; i < allColumns.length; i++) {
+                  const column = allColumns[i];
+                  totalColsWidth += column.getMinWidth();
+                  if (totalColsWidth > gridWidth) {
+                    columnsToHide.push(column.getColId());
+                  } else {
+                    columnsToShow.push(column.getColId());
+                  }
+                }
+              }
+              // show/hide columns based on current grid width
+              params.api.setColumnsVisible(columnsToShow, true);
+              params.api.setColumnsVisible(columnsToHide, false);
+              // wait until columns stopped moving and fill out
+              // any available space to ensure there are no gaps
+              window.setTimeout(() => {
+                params.api.sizeColumnsToFit();
+              }, 10);
+            },
+            [window],
+          );
+        
 
     
     return (
         // Data Grid will fill the size of the parent container
-        <div style={{ height: 500}}>
+        <div style={{ height:"100%" }}>
             <AgGridReact
                 rowData={rowData}
                 columnDefs={colDefs}
-                onCellKeyDown ={onCellValueChanged}
+                onCellKeyDown ={onCellValueChangedCallback}
+                onGridSizeChanged={onGridSizeChanged}
+                ref={gridRef}
+                theme={theme}
             />
         </div>
     )
